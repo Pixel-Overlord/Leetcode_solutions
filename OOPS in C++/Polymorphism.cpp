5070bacde139f107d2eeabbe90b3634f85b141a8
Satish Jha <jha.satish0210@gmail.com>
2026-05-14 18:16:34 +0530

    OOPS concept : Polymorphism

---
+++ OOPS in C++/Polymorphism.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void sound() {
+        cout << "Animal sound" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void sound() override {
+        cout << "Dog barks" << endl;
+    }
+};
+
+int main()
+{
+    Animal* ptr;
+
+    Dog d;
+    ptr = &d;
+
+    ptr->sound();
+
+    return 0;
+}
