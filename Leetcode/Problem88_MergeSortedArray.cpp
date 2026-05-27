f26650ea22757f6e4f1be9b36e4bacbd5e660f8c
Satish Jha <jha.satish0210@gmail.com>
2026-05-27 11:59:59 +0530

    88 : Merge sorted arrays

---
+++ Leetcode/Problem88_MergeSortedArray.cpp
@@ -0,0 +1,96 @@
+/*
+ * 88. Merge Sorted Array
+ * 
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+ * 
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ * 
+ * The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ * 
+ *  
+ * 
+ * Example 1:
+ * 
+ * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * Output: [1,2,2,3,5,6]
+ * Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+ * The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1. 
+ */
+
+ #include <iostream>
+ #include <vector>
+ #include <algorithm>
+ using namespace std;
+
+ class Solution {
+ public:
+
+    // Solution 1
+     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+         int i = 0, j = 0;
+         vector<int> temp;
+ 
+         if (n == 0) return;
+ 
+         while (i < n && j < n) {
+             if (nums1[i] <= nums2[j]) {
+                 temp.push_back(nums1[i]);
+                 i++;
+             } else {
+                 temp.push_back(nums2[j]);
+                 j++;
+             }
+         }
+ 
+         while (i < m) {
+            temp.push_back(nums1[i]);
+            i++;
+         }
+ 
+         while (j < n) {
+             temp.push_back(nums2[j]);
+             j++;
+         }
+ 
+         nums1 = temp;
+     }
+
+     // Solution 2
+     //void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+     //   int i = m - 1;
+     //   int j = n - 1;
+     //   int k = m + n - 1;
+     //
+     //   while (i >= 0 && j >= 0) {
+     //       if (nums1[i] > nums2[j]) {
+     //           nums1[k--] = nums1[i--];
+     //       } else {
+     //           nums1[k--] = nums2[j--];
+     //       }
+     //   }
+     //
+     //   while (j >= 0) {
+     //       nums1[k--] = nums2[j--];
+     //   }
+     }
+ };
+
+ int main () {
+    vector<int> nums1 = {1,2,3,0,0,0};
+    vector<int> nums2 = {2,5,6};
+
+    //vector<int> nums1 = {1};
+    //vector<int> nums2 = {};
+
+    int m = 3, n = 3;
+
+    Solution sol;
+
+    sol.merge(nums1, m, nums2, n);
+
+    cout << "Sorted array is : " << endl;
+    for (auto a: nums1) {
+        cout << a << " ";
+    }
+ }
+  
