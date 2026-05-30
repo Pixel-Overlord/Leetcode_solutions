9c7a531ec86b61d52d3b931e27156d11e288b31f
Satish Jha <jha.satish0210@gmail.com>
2026-05-30 12:48:41 +0530

    31 : Next Permutation

--- Leetcode/Array/Problem31_NextPermutation.cpp
+++ Leetcode/Array/Problem31_NextPermutation.cpp
@@ -35,13 +35,32 @@
 
  #include <iostream>
  #include <vector>
+ #include <algorithm>
  using namespace std;
 
  class Solution {
  public:     
      void nextPermutation(vector<int>& nums) {
-        
-     }
+        int n = nums.size();
+
+        // Step 1: Find pivot
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: Find next greater element and swap
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+
+        // Step 3: Reverse the suffix
+        reverse(nums.begin() + i + 1, nums.end());
+    }
  };
 
  int main() {
