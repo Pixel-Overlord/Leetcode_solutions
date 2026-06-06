807a83df61fe3c3120a2b5b4b5de7e5e647286cb
Satish Jha <jha.satish0210@gmail.com>
2026-06-06 13:57:21 +0530

    55 : Search insert position.

---
+++ Leetcode/Array/2_Pointer/Problem55_SearchInsertPosition.cpp
@@ -0,0 +1,61 @@
+/* 
+ * 35. Search Insert Position
+ *
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ * 
+ * You must write an algorithm with O(log n) runtime complexity.
+ * 
+ *  
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [1,3,5,6], target = 5
+ * Output: 2
+ * Example 2:
+ * 
+ * Input: nums = [1,3,5,6], target = 2
+ * Output: 1
+ * Example 3:
+ * 
+ * Input: nums = [1,3,5,6], target = 7
+ * Output: 4
+ */
+
+ #include <iostream>
+ #include <vector>
+ using namespace std;
+ 
+ class Solution {
+ public:
+     int searchInsert(vector<int>& nums, int target) {
+         int start = 0, end = nums.size() - 1;
+ 
+         while (start <= end) {
+             int mid = start + (end - start) / 2;
+
+             if (nums[mid] == target) {
+                return mid;
+             }
+
+             else if (nums[mid] < target) {
+                start = mid + 1;
+             }
+
+             else {
+                end = mid - 1;
+             }
+         }
+
+         return start;
+     }
+ };
+
+ int main() {
+     Solution sol;
+     vector<int> nums = {1,3,5,6};
+     int target = 2;
+
+     int ans = sol.searchInsert(nums, target);
+
+     cout << "The target is at or can be placed at : " << ans;
+ }
